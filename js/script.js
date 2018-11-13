0ca87c75987c34f930d3f1e43d4bad47e15a50ce
Rain Traagel <raintraagel@gmail.com>
2018-11-13 14:15:33 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+    	var typed = new Typed(".typed", {
+		strings: ["Infotehnoloogia Spetsialist.", "Veebi- ja Tarkvaraarendaja.", "Video- ja Fotograaf."],
+		typeSpeed: 70,
+		loop: true,
+		startDelay: 1000,
+		showCursor: false
+	});
+    
+});
